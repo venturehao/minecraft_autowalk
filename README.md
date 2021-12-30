f74f3e58e6929ab701b00fc3d74bad8cd3c8b4c1
venturehao <haow05@gmail.com>
2021-12-30 21:11:36 +0800

    update to support macos

--- README.md
+++ README.md
@@ -2,11 +2,22 @@
 a tiny tool to auto walk in mine craft
 
 
-目前只能适用于Win10系统 
+可适用于Win10系统 和 MacOS （仅测试了M1机型）
 
-使用方法：
+## Mac OS使用方法：
+1、安装了python3
 
 
+2、打开终端，运行:
+```
+pip3 install pynput pyobjc-framework-Quartz 
+```
+
+
+3、把仓库里的autowalk.py下载到本地，调节游戏画面走到合适的封闭位置之后按esc使游戏进入菜单模式，然后双击autowalk.py(如果让你选择使用什么程序打开，选择Python即可), 然后及时在游戏界面点击Back to Game，大概15秒后开始自动行走，enjoy your life...
+
+
+## Win10使用方法
 1、下载python安装包进行安装（目前测试过3.9和3.10），注意安装时选择Customized Install并勾选Add python path to environment
 
 
@@ -16,7 +27,8 @@ pip3 install pynput mouse
 ```
 然后回车等待结束
 
-3、把仓库里的autowalk.py下载到本地，调节游戏画面走到合适的封闭位置之后按esc使游戏进入菜单模式，然后双击autowalk.py(如果让你选择使用什么程序打开，选择Python即可), 然后及时在游戏界面点击Back to Game，大概15秒后开始自动行走，enjoy your life...
+
+3、和MacOS相同
 
 ##  乞讨地址 
 ### 0xf4f4c136def09530A0740785a00050Bcb756631e 
--- autowalk.py
+++ autowalk.py
@@ -1,11 +1,26 @@
-import time
+import time, platform, pynput
 from random import randrange
-import mouse
-from pynput.keyboard import Key, Controller
 
-keyboard = Controller()
+kc = pynput.keyboard.Controller()
+mc = pynput.mouse.Controller()
 start_wait = 15
 
+move_relative = None
+if "Darwin" in platform.system():
+    import Quartz
+    def osx_move_relative(dx, dy, delay=0.2):
+        ev = Quartz.CGEventCreateMouseEvent(None, Quartz.kCGEventMouseMoved, mc.position, Quartz.kCGMouseButtonLeft)
+        Quartz.CGEventSetIntegerValueField(ev, Quartz.kCGMouseEventDeltaX, dx)
+        Quartz.CGEventSetIntegerValueField(ev, Quartz.kCGMouseEventDeltaY, dy)
+        Quartz.CGEventPost(Quartz.kCGHIDEventTap, ev)
+        time.sleep(delay)
+    move_relative = osx_move_relative
+else:
+    import mouse
+    def win_move_relative(dx, dy, delay=0.2):
+        mouse._os_mouse.move_relative(dx, dy)
+        time.sleep(delay)
+    move_relative = win_move_relative
 
 direction = 1
 
@@ -14,18 +29,17 @@ def rand_walk():
     
     while True:
         print("Walk")
-        keyboard.press("w")
+        kc.press("w")
         time.sleep(randrange(5,10))
-        keyboard.release("w")
+        kc.release("w")
         
         
         direction = 0.6*direction + 0.4* (1-randrange(2)*2)
         strength = randrange(3,20)
         print("Turn direction: %f with strenth %d" % (direction, strength))
         for i in range(strength):
-            time.sleep(0.2)
-            mouse._os_mouse.move_relative(direction*50, 0)
+            move_relative(direction*50, 0)
 print("Wait {0} seconds to start auto walk, prepare your game ...".format(start_wait))
-time.sleep(15)
+time.sleep(start_wait)
 rand_walk()
 
